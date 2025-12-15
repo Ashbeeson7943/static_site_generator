63bd63b36f64ce1f2976bccb46fcd75b0de720be
Ashbeeson7943 <ashbeeson@hotmail.co.uk>
2025-12-15 12:26:56 +0000

    added text to textnodes

--- src/test_splitters.py
+++ src/test_splitters.py
@@ -1,6 +1,6 @@
 import unittest
 
-from splitters import split_nodes_delimiter, split_nodes_image, split_nodes_link
+from splitters import split_nodes_delimiter, split_nodes_image, split_nodes_link, text_to_textnodes
 from textnode import TextNode, TextType
 
 class TestSplitters(unittest.TestCase):
@@ -72,6 +72,15 @@ class TestSplitters(unittest.TestCase):
                                         TextNode(" and ", TextType.TEXT),
                                         TextNode("another link", TextType.LINK, "https://blog.boot.dev"),
                                         TextNode(" with text that follows", TextType.TEXT)])
+
+
+
+    def test_text_to_textnodes(self):
+        text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        nodes = text_to_textnodes(text)
+        self.assertListEqual(nodes, [TextNode("This is ", TextType.TEXT),TextNode("text", TextType.BOLD),TextNode(" with an ", TextType.TEXT),TextNode("italic", TextType.ITALIC),
+                                     TextNode(" word and a ", TextType.TEXT),TextNode("code block", TextType.CODE),TextNode(" and an ", TextType.TEXT),TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+                                     TextNode(" and a ", TextType.TEXT),TextNode("link", TextType.LINK, "https://boot.dev")])
 
 if __name__ == "__main__":
     unittest.main()
