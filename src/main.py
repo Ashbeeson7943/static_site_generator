168b9fe3e4e56c555330ad59bbe1f81cd182be22
Ashbeeson7943 <ashbeeson@hotmail.co.uk>
2025-12-18 16:48:03 +0000

    created the final steps to create the first static site

--- src/main.py
+++ src/main.py
@@ -1,12 +1,13 @@
 import os
 import shutil
-
+from converter import markdown_to_html_node
 
 def main():
     static_file_path = "./static"
     public_file_path = "./public"
     clear_public(public_file_path)
     copy_static_to_public(static_file_path, public_file_path)
+    generate_page("./content/index.md", "./template.html", os.path.join(public_file_path, "index.html"))
 
 def clear_public(base_path):
     if os.path.exists(base_path):
@@ -27,6 +28,29 @@ def copy_static_to_public(base_path, destination_path):
             print(f"Copying: {item_path} to {dest_path}")
             shutil.copy(item_path, dest_path)
 
+def extract_title(markdown):
+    md_lines = markdown.split("\n")
+    for line in md_lines:
+        if line.startswith("#"):
+            return line.split("#")[1].strip()
+    return "Title Not Found"
+
+
+
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    with open(from_path) as md:
+        markdown = md.read()
+    template = open(template_path).read()
+    html_string = markdown_to_html_node(markdown).to_html()
+    title = extract_title(markdown)
+    template = template.replace(r"{{ Title }}", title)
+    template = template.replace(r"{{ Content }}", html_string)
+
+    if not os.path.exists(dest_path):
+        os.makedirs(os.path.dirname(dest_path), exist_ok=True)
+        with open(dest_path,"w") as f:
+            f.write(template)
 
 
 main()
